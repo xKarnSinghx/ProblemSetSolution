57f6b50fa073082cb93195a008cb45da6d1b7c31
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-03-02 19:09:16 +0530

    Create BuyingTorch.cpp

---
+++ CF/BuyingTorch.cpp
@@ -0,0 +1,134 @@
+/*
+
+A. Buying Torches
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+You are playing a very popular game called Cubecraft. Initially, you have one stick and want to craft k torches. One torch can be crafted using one stick and one coal.
+
+Hopefully, you've met a very handsome wandering trader who has two trade offers:
+
+exchange 1 stick for x sticks (you lose 1 stick and gain x sticks).
+exchange y sticks for 1 coal (you lose y sticks and gain 1 coal).
+During one trade, you can use only one of these two trade offers. You can use each trade offer any number of times you want to, in any order.
+
+Your task is to find the minimum number of trades you need to craft at least k torches. The answer always exists under the given constraints.
+
+You have to answer t independent test cases.
+
+Input
+The first line of the input contains one integer t (1≤t≤2⋅104) — the number of test cases. Then t test cases follow.
+
+The only line of the test case contains three integers x, y and k (2≤x≤109; 1≤y,k≤109) — the number of sticks you can buy with one stick, the number of sticks required to buy one coal and the number of torches you need, respectively.
+
+Output
+For each test case, print the answer: the minimum number of trades you need to craft at least k torches. The answer always exists under the given constraints.
+
+Example
+inputCopy
+5
+2 1 5
+42 13 24
+12 11 12
+1000000000 1000000000 1000000000
+2 1000000000 1000000000
+outputCopy
+14
+33
+25
+2000000003
+1000000001999999999
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair<ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+#define MOD  1000000007;
+void solve();
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+	int t ; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+
+
+void solve()
+{
+	ll x, y, k;
+	cin >> x >> y >> k;
+	cout << ceils((y * k + k - 1), (x - 1)) + k;
+}
+
+
+
