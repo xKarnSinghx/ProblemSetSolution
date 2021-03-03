9e86719dba22a455562ce88eabdd94c7420884ae
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-03-03 22:48:47 +0530

    Create A^B^C.cpp

---
+++ ATC/A^B^C.cpp
@@ -0,0 +1,187 @@
+/*
+B - A^B^C  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+400
+ points
+
+Problem Statement
+Given positive integers 
+A
+,
+B
+,
+C
+, find the digit at the ones place in the decimal notation of 
+A
+B
+C
+.
+
+Constraints
+1
+≤
+A
+,
+B
+,
+C
+≤
+10
+9
+A
+,
+B
+,
+C
+ are integers.
+Input
+Input is given from Standard Input in the following format:
+
+A
+ 
+B
+ 
+C
+
+Output
+Print the digit at the one's place in the decimal notation of 
+A
+B
+C
+.
+
+Sample Input 1 
+Copy
+4 3 2
+Sample Output 1 
+Copy
+4
+The ones digit in the decimal notation of 
+4
+3
+2
+=
+4
+9
+=
+262144
+ is 
+4
+.
+
+Sample Input 2 
+Copy
+1 2 3
+Sample Output 2 
+Copy
+1
+Sample Input 3 
+Copy
+3141592 6535897 9323846
+Sample Output 3 
+Copy
+2
+
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair<ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+#define MOD  1000000007;
+void solve();
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+		solve();
+
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll mult(ll x, ll y, ll mod) {
+	return (x * y) % mod;
+}
+ll bin_pow(ll x, ll p, ll mod) {
+	if (p == 0) return 1;
+	if (p & 1) return mult(bin_pow(x, p - 1, mod), x, mod);
+	return bin_pow(mult(x, x, mod), p / 2, mod);
+}
+
+void solve()
+{
+
+	ll a, b, c;
+	cin >> a >> b >> c;
+	if (b > 1 && c > 1)
+		b = 4 + bin_pow(b % 4, c, 4);
+	a = bin_pow(a % 10, b, 10);
+	cout << a;
+
+}
+
+
+
