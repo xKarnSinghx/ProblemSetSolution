ff2cb1895276d2cfa579948a35d1736ecda7a39d
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-03-13 20:59:02 +0530

    Create HealthMDeath.cpp

---
+++ CF/HealthMDeath.cpp
@@ -0,0 +1,174 @@
+/*
+A - Health M Death  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+100
+ points
+
+Problem Statement
+Takahashi, the magician, is fighting with a monster.
+
+His magic can defeat a monster whose health is a multiple of 
+M
+. It has no effect on a monster whose health is not a multiple of 
+M
+.
+
+Can his magic defeat a monster whose health is 
+H
+?
+
+Constraints
+1
+≤
+M
+≤
+1000
+1
+≤
+H
+≤
+1000
+M
+ and 
+H
+ are integers.
+Input
+Input is given from Standard Input in the following format:
+
+M
+ 
+H
+
+Output
+If Takahashi's magic can defeat the monster, print Yes; otherwise, print No.
+
+Sample Input 1 
+Copy
+10 120
+Sample Output 1 
+Copy
+Yes
+Takahashi's magic can defeat a monster whose health is a multiple of 
+10
+.
+
+120
+ is a multiple of 
+10
+, so his magic can defeat the monster with health 
+120
+.
+
+Sample Input 2 
+Copy
+10 125
+Sample Output 2 
+Copy
+No
+125
+ is not a multiple of 
+10
+, so his magic cannot defeat the monster with health 
+125
+.
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair<ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+#define MOD  1000000007;
+void solve();
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+		solve();
+
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll mult(ll x, ll y, ll mod) {
+	return (x * y) % mod;
+}
+ll bin_pow(ll x, ll p, ll mod) {
+	if (p == 0) return 1;
+	if (p & 1) return mult(bin_pow(x, p - 1, mod), x, mod);
+	return bin_pow(mult(x, x, mod), p / 2, mod);
+}
+
+void solve()
+{
+
+	ll h, m;
+	cin >> m >> h;
+	if (h % m == 0)
+		cout << "Yes";
+	else
+		cout << "No";
+
+}
