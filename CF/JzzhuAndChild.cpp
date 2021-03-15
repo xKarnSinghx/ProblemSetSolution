ee667dc74bccc51cf329fc6172ba7f9803e4ec93
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-03-15 20:49:58 +0530

    Create JzzhuAndChild.cpp

---
+++ CF/JzzhuAndChild.cpp
@@ -0,0 +1,160 @@
+/*
+A. Jzzhu and Children
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+There are n children in Jzzhu's school. Jzzhu is going to give some candies to them. Let's number all the children from 1 to n. The i-th child wants to get at least ai candies.
+
+Jzzhu asks children to line up. Initially, the i-th child stands at the i-th place of the line. Then Jzzhu start distribution of the candies. He follows the algorithm:
+
+Give m candies to the first child of the line.
+If this child still haven't got enough candies, then the child goes to the end of the line, else the child go home.
+Repeat the first two steps while the line is not empty.
+Consider all the children in the order they go home. Jzzhu wants to know, which child will be the last in this order?
+
+Input
+The first line contains two integers n, m (1 ≤ n ≤ 100; 1 ≤ m ≤ 100). The second line contains n integers a1, a2, ..., an (1 ≤ ai ≤ 100).
+
+Output
+Output a single integer, representing the number of the last child.
+
+Examples
+inputCopy
+5 2
+1 3 1 4 2
+outputCopy
+4
+inputCopy
+6 4
+1 1 2 2 3 3
+outputCopy
+6
+Note
+Let's consider the first sample.
+
+Firstly child 1 gets 2 candies and go home. Then child 2 gets 2 candies and go to the end of the line. Currently the line looks like [3, 4, 5, 2] (indices of the children in order of the line). Then child 3 gets 2 candies and go home, and then child 4 gets 2 candies and goes to the end of the line. Currently the line looks like [5, 2, 4]. Then child 5 gets 2 candies and goes home. Then child 2 gets two candies and goes home, and finally child 4 gets 2 candies and goes home.
+
+Child 4 is the last one who goes home.
+
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair<ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+#define MOD  1000000007;
+void solve();
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+
+		solve();
+
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll mult(ll x, ll y, ll mod) {
+	return (x * y) % mod;
+}
+ll bin_pow(ll x, ll p, ll mod) {
+	if (p == 0) return 1;
+	if (p & 1) return mult(bin_pow(x, p - 1, mod), x, mod);
+	return bin_pow(mult(x, x, mod), p / 2, mod);
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)  return false;
+	if (n <= 3)  return true;
+	if (n % 2 == 0 || n % 3 == 0) return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+void solve()
+{
+	ll n, m;
+	cin >> n >> m;
+	ll a[n];
+	foi(n) {
+		cin >> a[i];
+		a[i] = ceils(a[i], m);
+	}
+	ll ans = 1, mx = -1;
+	foi(n) {
+		if (mx > a[i])
+			continue;
+		else {
+			mx = a[i];
+			ans = i + 1;
+		}
+	}
+	cout << ans;
+
+}
