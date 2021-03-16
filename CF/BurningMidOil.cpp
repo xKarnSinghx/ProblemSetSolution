6338da6e2ae5c4e52201c1b8a8436a258e886a2b
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-03-16 17:34:34 +0530

    Create BurningMidOil.cpp

---
+++ CF/BurningMidOil.cpp
@@ -0,0 +1,156 @@
+/*
+B. Burning Midnight Oil
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+One day a highly important task was commissioned to Vasya — writing a program in a night. The program consists of n lines of code. Vasya is already exhausted, so he works like that: first he writes v lines of code, drinks a cup of tea, then he writes as much as  lines, drinks another cup of tea, then he writes  lines and so on: , , , ...
+
+The expression  is regarded as the integral part from dividing number a by number b.
+
+The moment the current value  equals 0, Vasya immediately falls asleep and he wakes up only in the morning, when the program should already be finished.
+
+Vasya is wondering, what minimum allowable value v can take to let him write not less than n lines of code before he falls asleep.
+
+Input
+The input consists of two integers n and k, separated by spaces — the size of the program in lines and the productivity reduction coefficient, 1 ≤ n ≤ 109, 2 ≤ k ≤ 10.
+
+Output
+Print the only integer — the minimum value of v that lets Vasya write the program in one night.
+
+Examples
+inputCopy
+7 2
+outputCopy
+4
+inputCopy
+59 9
+outputCopy
+54
+Note
+In the first sample the answer is v = 4. Vasya writes the code in the following portions: first 4 lines, then 2, then 1, and then Vasya falls asleep. Thus, he manages to write 4 + 2 + 1 = 7 lines in a night and complete the task.
+
+In the second sample the answer is v = 54. Vasya writes the code in the following portions: 54, 6. The total sum is 54 + 6 = 60, that's even more than n = 59.
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair<ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+#define MOD  1000000007;
+void solve();
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+	//int t ; cin >> t;
+	//while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll mult(ll x, ll y, ll mod) {
+	return (x * y) % mod;
+}
+ll bin_pow(ll x, ll p, ll mod) {
+	if (p == 0) return 1;
+	if (p & 1) return mult(bin_pow(x, p - 1, mod), x, mod);
+	return bin_pow(mult(x, x, mod), p / 2, mod);
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)  return false;
+	if (n <= 3)  return true;
+	if (n % 2 == 0 || n % 3 == 0) return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+void solve()
+{
+	ll n, k;
+
+	cin >> n >> k;
+	ll t = n - n / k, s = t;;
+	for (ll i = 1; i < ceils(n, k); i++) {
+		s += t / pow(k, i);
+		if (t / pow(k, i + 1) < 1 and s < n )
+		{
+			t++;
+			s = t;
+			i = 0;
+		}
+		else if (s >= n)
+			break;
+	}
+	cout << t;
+
+}
