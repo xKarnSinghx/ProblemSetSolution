77b3e6583f266e661c108ee2aa99d7c89300a6b3
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-03-20 20:23:36 +0530

    Create IncArray.cpp

---
+++ CSES/IncArray.cpp
@@ -0,0 +1,165 @@
+/*
+Time limit: 1.00 s Memory limit: 512 MB
+You are given an array of n integers. You want to modify the array so that it is increasing, i.e., every element is at least as large as the previous element.
+
+On each move, you may increase the value of any element by one. What is the minimum number of moves required?
+
+Input
+
+The first input line contains an integer n: the size of the array.
+
+Then, the second line contains n integers x1,x2,…,xn: the contents of the array.
+
+Output
+
+Print the minimum number of moves.
+
+Constraints
+1≤n≤2⋅105
+1≤xi≤109
+Example
+
+Input:
+5
+3 2 5 1 7
+
+Output:
+5
+
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair<ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+const ll MOD = 1000000007;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+
+		solve();
+	
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll mult(ll x, ll y, ll mod) {
+	return (x * y) % mod;
+}
+ll bin_pow(ll x, ll p, ll mod) {
+	if (p == 0) return 1;
+	if (p & 1) return mult(bin_pow(x, p - 1, mod), x, mod);
+	return bin_pow(mult(x, x, mod), p / 2, mod);
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)  return false;
+	if (n <= 3)  return true;
+	if (n % 2 == 0 || n % 3 == 0) return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+ll fac(ll n) {
+	while (n % 4 == 0) {
+		n /= 4;
+	}
+	for (ll i = 3; i <= sqrt(n); i += 2) {
+		while (n % (i * i) == 0) {
+			n /= (i * i);
+		}
+	}
+	return n;
+}
+void solve()
+{
+	ll n;
+	cin >> n;
+	ll a[n];
+	foi(n)
+	cin >> a[i];
+	ll s = 0;
+	forr(i, 1, n) {
+		if (a[i ] >= a[i - 1])
+			continue;
+		else
+		{	s += a[i - 1] - a[i];
+			a[i] = a[i - 1];
+		}
+	}
+
+	cout << s;
+}
