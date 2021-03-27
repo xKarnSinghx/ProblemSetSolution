4f69930eeb4ad1e9f19c9308047b2d9c8d0dd286
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-03-27 22:40:50 +0530

    Create CandelDin.cpp

---
+++ CC/CandelDin.cpp
@@ -0,0 +1,363 @@
+/*
+Chef is having a candlelit dinner with his girlfriend at his place. Currently, it is time 0
+
+, Chef just managed to light a candle, but he is out of matchsticks.
+
+Since Chef has an infinite supply of (unlit) candles, he can use the following strategy: each second (starting at time 0
+), he takes one lit candle and lights up X other candles using it. This process takes 1 unit of time to complete, i.e. if Chef starts lighting up X candles at a time t1, he is able to finish at the time t1+1
+
+.
+
+However, since candle wax is limited, a candle can only burn for Y
+
+units of time and then it is extinguished.
+
+Chef's girlfriend arrives at a time A
+— specifically, after all the candles that were supposed to get extinguished at time A have been extinguished and after Chef lights up all the candles that he was lighting up since time A−1. Now Chef wants to know the maximum number of candles which can be lit at the time A
+
+to impress his girlfriend.
+Input
+
+    The first line of the input contains a single integer T
+
+denoting the number of test cases. The description of T
+test cases follows.
+The first and only line of each test case contains three space-separated integers A
+, Y and X
+
+    .
+
+Output
+
+For each test case, print a single line containing one integer — the maximum number of lit candles Chef can impress his girlfriend with.
+Constraints
+
+    1≤T≤105
+
+1≤A,Y,X≤109
+
+Subtasks
+
+Subtask #1 (40 points): A≥Y
+
+Subtask #2 (60 points): original constraints
+Example Input
+
+3
+1 1 1
+1 2 1
+2 2 1
+
+Example Output
+
+1
+2
+2
+
+Explanation
+
+Example case 1: At the time 0
+, there is one lit candle, using which we light 1 more candle. At the time 1, the starting candle gets extinguished and the candle which we started lighting up at the time 0 is lit, so we have only 1
+
+candle lit up when Chef's girlfriend arrives.
+
+Example case 2: At the time 0
+, there is one lit candle, using which we light 1 more candle. At the time 1, the candle which we started lighting up at the time 0
+
+and the starting candle are both lit.
+
+Example case 3: At the time 0
+, there is one lit candle, using which we light 1 more candle. At the time 1, the candle which we started lighting up at time 0 is lit, and we start lighting up one more candle. At the time 2, the starting candle gets extinguished and the candle which we started lighting up at the time 1 is lit. In total, we have 2 lit candles when Chef's girlfriend arrives.
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair<ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+const ll MOD = 1000000007;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+	ll t ; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll mult(ll x, ll y, ll mod) {
+	return (x * y) % mod;
+}
+ll bin_pow(ll x, ll p, ll mod) {
+	if (p == 0) return 1;
+	if (p & 1) return mult(bin_pow(x, p - 1, mod), x, mod);
+	return bin_pow(mult(x, x, mod), p / 2, mod);
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)  return false;
+	if (n <= 3)  return true;
+	if (n % 2 == 0 || n % 3 == 0) return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+vb is_prime(1e6 + 1, true);
+
+ll binomialCoeffSpaceEff(ll n, ll k)
+{
+	ll C[k + 1];
+	memset(C, 0, sizeof(C));
+
+	C[0] = 1; // nC0 is 1
+
+	for (ll i = 1; i <= n; i++) {
+		// Compute next row of pascal triangle using
+		// the previous row
+		for (ll j = min(i, k); j > 0; j--)
+			C[j] = (C[j] + C[j - 1]) % MOD;
+	}
+	return C[k];
+}
+ll primeFactors(ll n)
+{
+	vll ar;
+	ll i = 2;
+	while (n > 0)
+	{
+		if (i > n) break;
+		if (n % i == 0)
+		{
+			ar.pb(i);
+			while (n > 0 and n % i == 0) n = n / i;
+		}
+		i++;
+	}
+	return ar.size();
+}
+
+vpll primeFactorsFreq(ll n)
+{
+	vpll ar;
+	ll i = 2;
+	while (n > 0)
+	{
+		// cout<<n<<" ";
+		if (i > n) break;
+		if (n % i == 0)
+		{
+			ll cnt = 0;
+			while (n > 0 and n % i == 0) n = n / i, cnt++;
+			ar.pb(mp(i, cnt));
+		}
+		i++;
+	}
+	return ar;
+}
+void seive()
+{
+	is_prime[0] = is_prime[1] = false;
+	for (ll i = 2; i * i <= 1e6 + 1; i++) {
+		if (is_prime[i]) {
+			for (ll j = i * i; j <= 1e6 + 1; j += i)
+				is_prime[j] = false;
+		}
+	}
+}
+
+bool isSorted(vll &ar)
+{
+	ll n = ar.size();
+
+	for (ll i = 0; i < n - 1; i++)
+	{
+		if (ar[i + 1] < ar[i]) return false;
+	}
+
+	return true;
+}
+
+ll fastPower(ll x, ll y)
+{
+	ll res = 1;
+
+	while (y > 0)
+	{
+		// If y is odd, multiply x with result
+		if (y & 1)
+			res = (res * x) % MOD;
+
+		// y must be even now
+		y = y >> 1; // y = y/2
+		x = (x * x) % MOD; // Change x to x^2
+	}
+	return res;
+}
+
+ll kadanesAlgo(vll ar)
+{
+	ll n = ar.size();
+
+	ll currMax = 0;
+	ll mx = INT_MIN;
+
+	for (ll i = 0; i < n; i++)
+	{
+		currMax += ar[i];
+
+		if (currMax <= ar[i])
+		{
+			currMax = ar[i];
+		}
+
+		mx = max(currMax, mx);
+	}
+	return mx;
+}
+
+pll kadanesAlgoIdx(vll ar)
+{
+	ll n = ar.size();
+
+	ll currMax = 0;
+	ll mx = INT_MIN;
+	ll a = 0, b;
+	for (ll i = 0; i < n; i++)
+	{
+		currMax += ar[i];
+
+		if (currMax <= ar[i])
+		{
+			currMax = ar[i];
+			a = i;
+		}
+
+		if (mx < currMax)
+		{
+			mx = currMax;
+			b = i;
+		}
+	}
+	return mp(a, b);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+ll fac(ll n) {
+	while (n % 4 == 0) {
+		n /= 4;
+	}
+	for (ll i = 3; i <= sqrt(n); i += 2) {
+		while (n % (i * i) == 0) {
+			n /= (i * i);
+		}
+	}
+	return n;
+}
+ll findLCS(string a, string b, ll m, ll n)
+{
+
+	ll LCS[m + 1][n + 1];
+	ll result = 0;
+	for (ll i = 0; i <= m; i++)
+	{
+		for (ll j = 0; j <= n; j++)
+		{
+			if (i == 0 || j == 0)
+				LCS[i][j] = 0;
+
+			else if (a[i - 1] == b[j - 1]) {
+				LCS[i][j] = LCS[i - 1][j - 1] + 1;
+				result = max(result, LCS[i][j]);
+			}
+			else
+				LCS[i][j] = 0;
+		}
+	}
+	return result;
+}
+void solve()
+{
+	ll a, x, y;
+	cin >> a >> y >> x;
+	if (a >= y)
+	cout << x*y ;
+	else
+	cout << a*x + 1 ;
+
+}
+
+
