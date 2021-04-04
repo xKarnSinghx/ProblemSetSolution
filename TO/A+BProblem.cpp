33541bb651a682433c778f28012713d3ff6de73a
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-04-04 17:18:58 +0530

    Create A+BProblem.cpp

---
+++ TO/A+BProblem.cpp
@@ -0,0 +1,31 @@
+/*
+1000. A+B Problem
+Time limit: 1.0 second
+Memory limit: 64 MB
+Calculate a + b
+Input
+a and b
+Output
+a+b
+Sample
+input	output
+1 5
+6
+Notes
+Use + operator
+Problem Author: Pavel Atnashev
+*/
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+#define ll long long
+#define ld long double
+int main()
+{
+        ll a,b;
+        cin>>a>>b;
+        cout<<a+b;
+
+    return 0;
+}
