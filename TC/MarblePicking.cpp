18ae9ef6ae904da27b69a851255badbd511aac1b
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-04-10 23:48:38 +0530

    Create MarblePicking.cpp

---
+++ TC/MarblePicking.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair<int, pii>      piii;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+class  MarblePicking {
+public:
+int fewestColors(vector <string> marbles, int count){
+int c[27]={0};
+    string s="";
+    for(int i=0;i<marbles.size();i++)
+       { s+=marbles[i];}
+    for(int i=0;i<s.size();i++)
+        {c[s[i]-'A']++;}
+    sort(c,c+27);
+    int x=0,y=count;
+    for(int i=26;i>-1;i--){
+    if(y<=0)
+        break;
+     else if(y>0){
+        y-=c[i];
+            x++;
+        }
+    }
+return x;
+}
+};
