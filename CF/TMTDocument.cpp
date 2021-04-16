b908ec896d8545aeb9fb043fed79cb518ce3799b
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-04-16 23:42:16 +0530

    Create TMTDocument.cpp

---
+++ CF/TMTDocument.cpp
@@ -0,0 +1,478 @@
+/*
+B. TMT Document
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+The student council has a shared document file. Every day, some members of the student council write the sequence TMT (short for Towa Maji Tenshi) in it.
+
+However, one day, the members somehow entered the sequence into the document at the same time, creating a jumbled mess. Therefore, it is Suguru Doujima's task to figure out whether the document has malfunctioned. Specifically, he is given a string of length n whose characters are all either T or M, and he wants to figure out if it is possible to partition it into some number of disjoint subsequences, all of which are equal to TMT. That is, each character of the string should belong to exactly one of the subsequences.
+
+A string a is a subsequence of a string b if a can be obtained from b by deletion of several (possibly, zero) characters.
+
+Input
+The first line contains an integer t (1≤t≤5000)  — the number of test cases.
+
+The first line of each test case contains an integer n (3≤n<105), the number of characters in the string entered in the document. It is guaranteed that n is divisible by 3.
+
+The second line of each test case contains a string of length n consisting of only the characters T and M.
+
+It is guaranteed that the sum of n over all test cases does not exceed 105.
+
+Output
+For each test case, print a single line containing YES if the described partition exists, and a single line containing NO otherwise.
+
+Example
+inputCopy
+5
+3
+TMT
+3
+MTT
+6
+TMTMTT
+6
+TMTTTT
+6
+TTMMTT
+outputCopy
+YES
+NO
+YES
+NO
+YES
+Note
+In the first test case, the string itself is already a sequence equal to TMT.
+
+In the third test case, we may partition the string into the subsequences TMTMTT. Both the bolded and the non-bolded subsequences are equal to TMT.
+
+
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair<pll, ll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+const ll MOD = 1000000007;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+	ll t ; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll mult(ll x, ll y, ll mod) {
+	return (x * y) % mod;
+}
+ll bin_pow(ll x, ll p, ll mod) {
+	if (p == 0) return 1;
+	if (p & 1) return mult(bin_pow(x, p - 1, mod), x, mod);
+	return bin_pow(mult(x, x, mod), p / 2, mod);
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)  return false;
+	if (n <= 3)  return true;
+	if (n % 2 == 0 || n % 3 == 0) return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+vb is_prime(1e6 + 1, true);
+
+ll binomialCoeffSpaceEff(ll n, ll k)
+{
+	ll C[k + 1];
+	memset(C, 0, sizeof(C));
+
+	C[0] = 1; // nC0 is 1
+
+	for (ll i = 1; i <= n; i++) {
+		// Compute next row of pascal triangle using
+		// the previous row
+		for (ll j = min(i, k); j > 0; j--)
+			C[j] = (C[j] + C[j - 1]) % MOD;
+	}
+	return C[k];
+}
+ll primeFactors(ll n)
+{
+	vll ar;
+	ll i = 2;
+	while (n > 0)
+	{
+		if (i > n) break;
+		if (n % i == 0)
+		{
+			ar.pb(i);
+			while (n > 0 and n % i == 0) n = n / i;
+		}
+		i++;
+	}
+	return ar.size();
+}
+
+vpll primeFactorsFreq(ll n)
+{
+	vpll ar;
+	ll i = 2;
+	while (n > 0)
+	{
+		// cout<<n<<" ";
+		if (i > n) break;
+		if (n % i == 0)
+		{
+			ll cnt = 0;
+			while (n > 0 and n % i == 0) n = n / i, cnt++;
+			ar.pb(mp(i, cnt));
+		}
+		i++;
+	}
+	return ar;
+}
+void seive()
+{
+	is_prime[0] = is_prime[1] = false;
+	for (ll i = 2; i * i <= 1e6 + 1; i++) {
+		if (is_prime[i]) {
+			for (ll j = i * i; j <= 1e6 + 1; j += i)
+				is_prime[j] = false;
+		}
+	}
+}
+
+bool isSorted(vll &ar)
+{
+	ll n = ar.size();
+
+	for (ll i = 0; i < n - 1; i++)
+	{
+		if (ar[i + 1] < ar[i]) return false;
+	}
+
+	return true;
+}
+
+ll fastPower(ll x, ll y)
+{
+	ll res = 1;
+
+	while (y > 0)
+	{
+		// If y is odd, multiply x with result
+		if (y & 1)
+			res = (res * x) % MOD;
+
+		// y must be even now
+		y = y >> 1; // y = y/2
+		x = (x * x) % MOD; // Change x to x^2
+	}
+	return res;
+}
+
+ll kadanesAlgo(vll ar)
+{
+	ll n = ar.size();
+
+	ll currMax = 0;
+	ll mx = INT_MIN;
+
+	for (ll i = 0; i < n; i++)
+	{
+		currMax += ar[i];
+
+		if (currMax <= ar[i])
+		{
+			currMax = ar[i];
+		}
+
+		mx = max(currMax, mx);
+	}
+	return mx;
+}
+
+pll kadanesAlgoIdx(vll ar)
+{
+	ll n = ar.size();
+	ll currMax = 0;
+	ll mx = INT_MIN;
+	ll a = 0, b;
+	for (ll i = 0; i < n; i++)
+	{
+		currMax += ar[i];
+
+		if (currMax <= ar[i])
+		{
+			currMax = ar[i];
+			a = i;
+		}
+
+		if (mx < currMax)
+		{
+			mx = currMax;
+			b = i;
+		}
+	}
+	return mp(a, b);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+ll fac(ll n) {
+	while (n % 4 == 0) {
+		n /= 4;
+	}
+	for (ll i = 3; i <= sqrt(n); i += 2) {
+		while (n % (i * i) == 0) {
+			n /= (i * i);
+		}
+	}
+	return n;
+}
+ll findLCS(string a, string b, ll m, ll n)
+{
+
+	ll LCS[m + 1][n + 1];
+	ll result = 0;
+	for (ll i = 0; i <= m; i++)
+	{
+		for (ll j = 0; j <= n; j++)
+		{
+			if (i == 0 || j == 0)
+				LCS[i][j] = 0;
+
+			else if (a[i - 1] == b[j - 1]) {
+				LCS[i][j] = LCS[i - 1][j - 1] + 1;
+				result = max(result, LCS[i][j]);
+			}
+			else
+				LCS[i][j] = 0;
+		}
+	}
+	return result;
+}
+ll add(ll x, ll y) {
+	x += y;
+	if (x >= MOD) return x - MOD;
+	return x;
+}
+ll sub(ll x, ll y) {
+	x -= y;
+	if (x < 0) return x + MOD;
+	return x;
+}
+ll f(ll x) {
+	ll d = 0;
+	while (x > 0)
+	{
+		d += x % 10;
+		x /= 10;
+	}
+
+	return d;
+}
+vs generateGray(ll n)
+{
+	if (n <= 0)
+		return {"0"};
+
+	if (n == 1)
+	{
+		return {"0", "1"};
+	}
+	vs recAns = generateGray(n - 1);
+	vs mainAns;
+	for (ll i = 0; i < recAns.size(); i++)
+	{
+		string s = recAns[i];
+		mainAns.push_back("0" + s);
+	}
+	for (ll i = recAns.size() - 1; i >= 0; i--)
+	{
+		string s = recAns[i];
+		mainAns.push_back("1" + s);
+	}
+	return mainAns;
+}
+void generateGraya(ll n)
+{
+	vs a;
+	a = generateGray(n);
+	for (ll i = 0 ; i < a.size(); i++ )
+		cout << a[i] << endl;
+}
+void move(ll disks, ll source, ll auxiliary, ll target)
+{
+	if (disks > 0)
+	{
+		move(disks - 1, source, target, auxiliary);
+		cout << source << "  " << target << endl;
+		move(disks - 1, auxiliary, source, target);
+	}
+}
+ld polyarea(ld n, ld a)
+{
+	if (a < 0 && n < 0)
+		return -1;
+
+	ld A = (a * a * n) / (4 * tan((180 / n) * 3.14159 / 180));
+
+	return A;
+}
+ll eqDiv(ll *a, ll s1, ll s, ll l) {
+	if (l == 0)
+		return abs((s - s1) - s1);
+
+	return min(eqDiv(a, s1 + a[l], s, l - 1), eqDiv(a, s1, s, l - 1));
+
+}
+ll noofdigit(ll n) {
+	ll x = 0;
+	while (n > 0) {
+		n /= 10;
+		x++;
+	}
+	return x;
+}
+ll factorial(ll n)
+{
+	if (n == 0)
+		return 1;
+	return (n * factorial(n - 1) % MOD) % MOD;
+}
+void solve()
+{
+	ll n;
+	cin >> n;
+	string s;
+	cin >> s;
+	ll t = 0, t1 = 0, m = 0, m1 = 0;
+	ll tx = 0, t1x = 0, mx = 0, m1x = 0;
+	if (s[0] == 'M' or s[n - 1] == 'M') {
+		cout << "NO";
+		return;
+	}
+	foi(n) {
+		if (s[i] == 'T')
+			t++, t1++;
+		else
+			m++, m1++;
+		if (m > 0 and t > 0) {
+			t--;
+			m--;
+			if (t < 0) {
+				cout << "NO";
+				return;
+			}
+		}
+		if (m1 > t1) {
+			cout << "NO";
+			return;
+
+		}
+	}
+	for (ll i = n - 1; i > -1; i--) {
+		if (s[i] == 'T')
+			tx++, t1x++;
+		else
+			mx++, m1x++;
+		if (mx > 0 and tx > 0) {
+			tx--;
+			mx--;
+			if (tx < 0) {
+				cout << "NO";
+				return;
+			}
+		}
+		if (m1x > t1x) {
+			cout << "NO";
+			return;
+
+		}
+	}
+	if (t1 == 2 * m1)
+		cout << "YES";
+	else
+		cout << "NO";
+}
+
+
+
+ 
