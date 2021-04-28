8cffd3b4f1334fcc74c920785198ceda65d7b639
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-04-28 23:44:48 +0530

    Create BinProto.cpp

---
+++ CF/BinProto.cpp
@@ -0,0 +1,156 @@
+/*
+A. Binary Protocol
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Polycarp has just invented a new binary protocol for data transmission. He is encoding positive integer decimal number to binary string using following algorithm:
+
+Each digit is represented with number of '1' characters equal to the value of that digit (for 0 it is zero ones).
+Digits are written one by one in order corresponding to number and separated by single '0' character.
+Though Polycarp learnt how to encode the numbers, he has no idea how to decode them back. Help him calculate the decoded number.
+
+Input
+The first line contains one integer number n (1 ≤ n ≤ 89) — length of the string s.
+
+The second line contains string s — sequence of '0' and '1' characters, number in its encoded format. It is guaranteed that the number corresponding to the string is positive and doesn't exceed 109. The string always starts with '1'.
+
+Output
+Print the decoded number.
+
+Examples
+inputCopy
+3
+111
+outputCopy
+3
+inputCopy
+9
+110011101
+outputCopy
+2031
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+const ll MOD = 1000000007;
+ll TT = 1;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+
+//	ll tc ; cin >> tc;
+//	while (tc--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+ll sod(ll n) {
+	ll s = 0;
+	while (n > 0) {
+		s += n % 10;
+		n /= 10;
+	}
+	return s;
+}
+void solve()
+{
+	ll n;
+	string s;
+	cin >> n >> s;
+	ll s1 = 0;
+	foi(n) {
+		if (s[i] == '1')
+			s1++;
+		else if (s[i] == '0') {
+			cout << s1;
+			s1 = 0;
+			if (i == n - 1)
+				cout << s1;
+		}
+	}
+	if (s1)
+		cout << s1;
+
+
+
+}
+
+ 
