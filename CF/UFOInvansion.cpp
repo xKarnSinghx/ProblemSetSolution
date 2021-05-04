7129f4c4a04ea911bc86e499242e65d38ba1e305
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-04 20:17:08 +0530

    Create UFOInvansion.cpp

---
+++ CF/UFOInvansion.cpp
@@ -0,0 +1,191 @@
+/*
+A - UFO Invasion  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+100
+ points
+
+Problem Statement
+You are given a string 
+S
+ of length 
+12
+. How many occurrences of ZONes does it contain as contiguous substrings?
+
+Constraints
+S
+ is a string of length 
+12
+ consisting of English letters.
+Input
+Input is given from Standard Input in the following format:
+
+S
+
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+const ll MOD = 1000000007;
+ll TT = 1;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+
+	//ll t ; cin >> t;
+	//while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
+void computeLPSArray(string pat, int M,
+                     int lps[])
+{
+	int len = 0;
+	int i = 1;
+	lps[0] = 0; 
+	while (i < M)
+	{
+		if (pat[i] == pat[len])
+		{
+			len++;
+			lps[i] = len;
+			i++;
+		}
+		else 
+		{
+			if (len != 0)
+			{
+				len = lps[len - 1];
+			}
+			else 
+			{
+				lps[i] = len;
+				i++;
+			}
+		}
+	}
+}
+
+int KMPSearch(string pat, string txt)
+{
+	int M = pat.length();
+	int N = txt.length();
+	int lps[M];
+	int j = 0; 
+	computeLPSArray(pat, M, lps);
+
+	int i = 0; 
+	int res = 0;
+	int next_i = 0;
+
+	while (i < N)
+	{
+		if (pat[j] == txt[i])
+		{
+			j++;
+			i++;
+		}
+		if (j == M)
+		{
+			j = lps[j - 1];
+			res++;
+		}
+		else if (i < N && pat[j] != txt[i])
+		{
+			if (j != 0)
+				j = lps[j - 1];
+			else
+				i = i + 1;
+		}
+	}
+	return res;
+}
+void solve()
+{
+	string s;
+	cin >> s;
+	ll x = KMPSearch("ZONe", s);
+	cout << x;
+
+}
+
+
