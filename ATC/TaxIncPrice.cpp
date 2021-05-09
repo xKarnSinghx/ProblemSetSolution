6f80bd74b34941b7b3606933244e2b3b5013791f
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-09 23:56:30 +0530

    Create TaxIncPrice.cpp

---
+++ ATC/TaxIncPrice.cpp
@@ -0,0 +1,54 @@
+/*
+A - Tax Included Price  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+The consumption tax rate in the Republic of ARC is 
+t
+ percent, where 
+t
+ is a positive integer.
+
+There is a shop called Seisu-ya (integer shop) there. It sells each positive integer 
+A
+ for 
+A
+ yen (Japanese currency) excluding tax, that is, 
+⌊
+100
++
+t
+100
+A
+⌋
+ yen including tax. Here, 
+⌊
+x
+⌋
+ denotes the largest integer not greater than 
+x
+ for a real number 
+x
+.
+
+Although Seisu-ya sells every positive integer, there are some positive integer values that cannot be the tax-included price of an integer. Among those values, find the 
+N
+-th smallest value.
+
+Constraints
+1
+≤
+t
+≤
+50
+1
+≤
+N
+≤
+10
+9
+*/
