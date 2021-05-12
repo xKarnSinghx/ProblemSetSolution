0637821da307ce9f90437e78b72d164962b34312
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-12 23:57:32 +0530

    Create AlmostGcd.cpp

---
+++ ATC/AlmostGcd.cpp
@@ -0,0 +1,63 @@
+/*
+B - Almost GCD  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+Given is an integer sequence 
+A
+: 
+A
+1
+,
+A
+2
+,
+A
+3
+,
+…
+,
+A
+N
+.
+Let the GCD-ness of a positive integer 
+k
+ be the number of elements among 
+A
+1
+,
+A
+2
+,
+A
+3
+,
+…
+,
+A
+N
+ that are divisible by 
+k
+.
+Among the integers greater than or equal to 
+2
+, find the integer with the greatest GCD-ness. If there are multiple such integers, you may print any of them.
+
+Constraints
+1
+≤
+N
+≤
+100
+2
+≤
+A
+i
+≤
+1000
+All values in input are integers.
+*/
