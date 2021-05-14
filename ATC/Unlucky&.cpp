d9868750216ca63af3a937796a4532889afdef9f
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-15 04:17:02 +0530

    Create Unlucky&.cpp

---
+++ ATC/Unlucky&.cpp
@@ -0,0 +1,161 @@
+/*
+C - Unlucky 7  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+Takahashi hates the number 
+7
+.
+
+We are interested in integers without the digit 
+7
+ in both decimal and octal. How many such integers are there between 
+1
+ and 
+N
+ (inclusive)?
+
+Constraints
+1
+≤
+N
+≤
+10
+5
+N
+ is an integer.
+ */
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+const ll MOD = 1000000007;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+
+//	ll t ; cin >> t;
+//	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+
+}
+string to_oct(ll n) {
+	string s;
+	while (n) {
+		s = to_string(n % 8) + s;
+		n /= 8;
+	}
+	return s;
+}
+void solve() {
+	ll n;
+	cin >> n;
+	ll ans = 0;
+	for (ll i = 1; i <= n; i++) {
+		ll a = i, f = 0, f1 = 0;
+		while (a > 0) {
+			if (a % 10 == 7) {
+				f = 1;
+				break;
+			}
+			a /= 10;
+		}
+		a = i;
+		while (a > 0) {
+			if (a % 8 == 7) {
+				f1 = 1;
+				break;
+			}
+			a /= 8;
+		}
+		if (f or f1)
+			ans++;
+	}
+	cout << n - ans;
+
+}
+
