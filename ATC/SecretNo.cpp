b258cc32bf910f763ba90c3caaa63ef0e47f8c7d
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-15 23:56:10 +0530

    Create SecretNo.cpp

---
+++ ATC/SecretNo.cpp
@@ -0,0 +1,52 @@
+/*
+C - Secret Number  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+Takahashi has forgotten his PIN. The PIN is a four-digit string consisting of 0, 1, 
+…
+, 9, and may begin with a 0.
+
+For each digit 0 through 9, Takahashi remembers the following fact, represented by a 
+10
+-character string 
+S
+0
+S
+1
+…
+S
+9
+:
+
+if 
+S
+i
+ is o: he is certain that the PIN contained the digit 
+i
+;
+if 
+S
+i
+ is x: he is certain that the PIN did not contain the digit 
+i
+;
+if 
+S
+i
+ is ?: he is not sure whether the PIN contained the digit 
+i
+.
+How many strings are there that could be Takahashi's PIN?
+
+Constraints
+S
+ is a 
+10
+-character string consisting of o, x, and ?.
+
+*/
