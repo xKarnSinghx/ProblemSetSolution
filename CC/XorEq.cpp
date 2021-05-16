ed8ef614ebc43de6107a6b18bd7a7c4e5e3b8d0f
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-16 23:53:46 +0530

    Create XorEq.cpp

---
+++ CC/XorEq.cpp
@@ -0,0 +1,29 @@
+/*
+For a given N, find the number of ways to choose an integer x from the range [0,2N−1] such that x⊕(x+1)=(x+2)⊕(x+3), where ⊕
+
+denotes the bitwise XOR operator.
+
+Since the number of valid x
+can be large, output it modulo 109+7
+
+.
+Input
+
+    The first line contains an integer T
+
+, the number of test cases. Then the test cases follow.
+The only line of each test case contains a single integer N
+
+    .
+
+Output
+
+For each test case, output in a single line the answer to the problem modulo 109+7
+
+.
+Constraints
+
+    1≤T≤105
+
+1≤N≤105
+*/
