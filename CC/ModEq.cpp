ea4a24414cd60ebf981ebb426b2870f0730ff8cd
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-16 23:55:40 +0530

    Create ModEq.cpp

---
+++ CC/ModEq.cpp
@@ -0,0 +1,26 @@
+/*
+Given integers N and M, find the number of ordered pairs (a,b) such that 1≤a<b≤N and ((M mod a) mod b)=((M mod b) mod a)
+
+.
+Input
+
+    The first line contains an integer T
+
+, the number of test cases. Then the test cases follow.
+The only line of each test case contains two integers N
+, M
+
+    .
+
+Output
+
+For each testcase, output in a single line the answer to the problem.
+Constraints
+
+    1≤T≤1000
+
+2≤N≤106
+1≤M≤5⋅105
+The sum of N
+over all test cases does not exceed 106.
+*/
