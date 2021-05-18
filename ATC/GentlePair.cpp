08252accbb5a84918a524af82200d3b910d1f898
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-18 23:59:05 +0530

    Create GentlePair.cpp

---
+++ ATC/GentlePair.cpp
@@ -0,0 +1,89 @@
+/*
+B - Gentle Pairs  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+On the 
+x
+y
+-plane, We have 
+N
+ points numbered 
+1
+ to 
+N
+. Point 
+i
+ is at 
+(
+x
+i
+,
+y
+i
+)
+, and the 
+x
+-coordinates of the 
+N
+ points are pairwise different.
+
+Find the number of pairs of integers 
+(
+i
+,
+j
+)
+ 
+(
+i
+<
+j
+)
+ that satisfy the following condition:
+
+The line passing through Point 
+i
+ and Point 
+j
+ has a slope between 
+−
+1
+ and 
+1
+ (inclusive).
+Constraints
+All values in input are integers.
+1
+≤
+N
+≤
+10
+3
+|
+x
+i
+|
+,
+|
+y
+i
+|
+≤
+10
+3
+x
+i
+≠
+x
+j
+ for 
+i
+≠
+j
+.
+*/
