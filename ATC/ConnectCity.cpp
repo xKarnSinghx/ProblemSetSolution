0b2bfa257c3c7dc47284e6f89b1e3b27660e4e91
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-20 03:48:04 +0530

    Create ConnectCity.cpp

---
+++ ATC/ConnectCity.cpp
@@ -0,0 +1,66 @@
+/*
+C - Connect Cities  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+There are 
+N
+ cities numbered 
+1
+ through 
+N
+, and 
+M
+ bidirectional roads numbered 
+1
+ through 
+M
+. Road 
+i
+ connects City 
+A
+i
+ and City 
+B
+i
+.
+
+Snuke can perform the following operation zero or more times:
+
+Choose two distinct cities that are not directly connected by a road, and build a new road between the two cities.
+After he finishes the operations, it must be possible to travel from any city to any other cities by following roads (possibly multiple times).
+
+What is the minimum number of roads he must build to achieve the goal?
+
+Constraints
+2
+≤
+N
+≤
+100
+,
+000
+1
+≤
+M
+≤
+100
+,
+000
+1
+≤
+A
+i
+<
+B
+i
+≤
+N
+No two roads connect the same pair of cities.
+All values in input are integers.
+
+*/
