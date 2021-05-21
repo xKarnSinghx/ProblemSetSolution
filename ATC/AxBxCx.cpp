bd5a9f8f326944427c7c70d62d258c314586468c
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-21 23:50:15 +0530

    Create AxBxCx.cpp

---
+++ ATC/AxBxCx.cpp
@@ -0,0 +1,39 @@
+/*
+C - A x B + C  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+Given is a positive integer 
+N
+. How many tuples 
+(
+A
+,
+B
+,
+C
+)
+ of positive integers satisfy 
+A
+×
+B
++
+C
+=
+N
+?
+
+Constraints
+2
+≤
+N
+≤
+10
+6
+All values in input are integers.
+
+*/
