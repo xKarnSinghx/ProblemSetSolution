7b69d38e3568a4944885164afc7b142cb096ee59
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-21 23:54:08 +0530

    Create ProdMax.cpp

---
+++ ATC/ProdMax.cpp
@@ -0,0 +1,61 @@
+/*
+B - Product Max  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+Given are integers 
+a
+,
+b
+,
+c
+ and 
+d
+. If 
+x
+ and 
+y
+ are integers and 
+a
+≤
+x
+≤
+b
+ and 
+c
+≤
+y
+≤
+d
+ hold, what is the maximum possible value of 
+x
+×
+y
+?
+
+Constraints
+−
+10
+9
+≤
+a
+≤
+b
+≤
+10
+9
+−
+10
+9
+≤
+c
+≤
+d
+≤
+10
+9
+All values in input are integers.*/
