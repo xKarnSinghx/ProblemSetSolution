51029bfc8a0cb57a33876b9221968e630146b0c3
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-21 23:26:59 +0530

    Create Mul@Div6.cpp

---
+++ CF/Mul@Div6.cpp
@@ -0,0 +1,171 @@
+/*
+B. Multiply by 2, divide by 6
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+You are given an integer n. In one move, you can either multiply n by two or divide n by 6 (if it is divisible by 6 without the remainder).
+
+Your task is to find the minimum number of moves needed to obtain 1 from n or determine if it's impossible to do that.
+
+You have to answer t independent test cases.
+
+Input
+The first line of the input contains one integer t (1≤t≤2⋅104) — the number of test cases. Then t test cases follow.
+
+The only line of the test case contains one integer n (1≤n≤109).
+
+Output
+For each test case, print the answer — the minimum number of moves needed to obtain 1 from n if it's possible to do that or -1 if it's impossible to obtain 1 from n.
+
+Example
+inputCopy
+7
+1
+2
+3
+12
+12345
+15116544
+387420489
+outputCopy
+0
+-1
+2
+-1
+-1
+12
+36
+Note
+Consider the sixth test case of the example. The answer can be obtained by the following sequence of moves from the given integer 15116544:
+
+Divide by 6 and get 2519424;
+divide by 6 and get 419904;
+divide by 6 and get 69984;
+divide by 6 and get 11664;
+multiply by 2 and get 23328;
+divide by 6 and get 3888;
+divide by 6 and get 648;
+divide by 6 and get 108;
+multiply by 2 and get 216;
+divide by 6 and get 36;
+divide by 6 and get 6;
+divide by 6 and get 1.
+
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<ll, vll>             mvll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+const ll MOD = 1000000007;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+
+	ll t ; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+
+}
+void solve() {
+	ll n;
+	cin >> n;
+	mll m;
+	ll ans = 0;
+	while (n != 1) {
+		if (n % 6 == 0)
+			n /= 6;
+		else
+			n *= 2;
+		ans++;
+		if (m[n]) {
+			cout << -1;
+			return;
+		}
+		else
+			m[n]++;
+	}
+	cout << ans;
+}
+
