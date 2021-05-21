3b764d5829f253747a56a94a9ae5c5e1c6b58666
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-21 23:54:34 +0530

    Create Ubiquity.cpp

---
+++ ATC/Ubiquity.cpp
@@ -0,0 +1,61 @@
+/*
+C - Ubiquity  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+How many integer sequences 
+A
+1
+,
+A
+2
+,
+…
+,
+A
+N
+ of length 
+N
+ satisfy all of the following conditions?
+
+0
+≤
+A
+i
+≤
+9
+There exists some 
+i
+ such that 
+A
+i
+=
+0
+ holds.
+There exists some 
+i
+ such that 
+A
+i
+=
+9
+ holds.
+The answer can be very large, so output it modulo 
+10
+9
++
+7
+.
+
+Constraints
+1
+≤
+N
+≤
+10
+6
+*/
