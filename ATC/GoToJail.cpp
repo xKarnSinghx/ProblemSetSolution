5c1c4a72bafa752e050f72268129aabb873ca2b6
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-21 23:49:47 +0530

    Create GoToJail.cpp

---
+++ ATC/GoToJail.cpp
@@ -0,0 +1,83 @@
+/*
+B - Go to Jail  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+Tak performed the following action 
+N
+ times: rolling two dice. The result of the 
+i
+-th roll is 
+D
+i
+,
+1
+ and 
+D
+i
+,
+2
+.
+
+Check if doublets occurred at least three times in a row. Specifically, check if there exists at lease one 
+i
+ such that 
+D
+i
+,
+1
+=
+D
+i
+,
+2
+, 
+D
+i
++
+1
+,
+1
+=
+D
+i
++
+1
+,
+2
+ and 
+D
+i
++
+2
+,
+1
+=
+D
+i
++
+2
+,
+2
+ hold.
+
+Constraints
+3
+≤
+N
+≤
+100
+1
+≤
+D
+i
+,
+j
+≤
+6
+All values in input are integers.
+*/
