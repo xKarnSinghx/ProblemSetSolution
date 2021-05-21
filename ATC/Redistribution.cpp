e633285f4b05df7a1588f1586f366261223ea189
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-21 23:55:03 +0530

    Create Redistribution.cpp

---
+++ ATC/Redistribution.cpp
@@ -0,0 +1,29 @@
+/*
+D - Redistribution  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+400
+ points
+
+Problem Statement
+Given is an integer 
+S
+. Find how many sequences there are whose terms are all integers greater than or equal to 
+3
+, and whose sum is equal to 
+S
+. The answer can be very large, so output it modulo 
+10
+9
++
+7
+.
+
+Constraints
+1
+≤
+S
+≤
+2000
+All values in input are integers.*/
