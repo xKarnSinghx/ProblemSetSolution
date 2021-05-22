622b5ea58f914df1fbb4226a49adbd2f720fef6d
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-22 23:56:51 +0530

    Create AAB.cpp

---
+++ ATC/AAB.cpp
@@ -0,0 +1,47 @@
+/*
+D - aab aba baa  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+400
+ points
+
+Problem Statement
+Among the strings of length 
+A
++
+B
+ containing 
+A
+ occurrences of a and 
+B
+ occurrences of b, find the string that comes 
+K
+-th in the lexicographical order.
+
+Constraints
+1
+≤
+A
+,
+B
+≤
+30
+1
+≤
+K
+≤
+S
+, where 
+S
+ is the number of strings of length 
+A
++
+B
+ containing 
+A
+ occurrences of a and 
+B
+ occurrences of b.
+All values in input are integers.
+*/
