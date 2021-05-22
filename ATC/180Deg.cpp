cd9a7b29f83d07b04dfc7214acad744752505b28
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-22 23:55:56 +0530

    Create 180Deg.cpp

---
+++ ATC/180Deg.cpp
@@ -0,0 +1,38 @@
+/*
+B - 180°  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+You are given a string 
+S
+ consisting of 0, 1, 6, 8, and 9.
+
+Rotate 
+S
+ 
+180
+ degrees and print the result. In other words, apply the following operations on 
+S
+ and print the resulting string:
+
+Reverse 
+S
+.
+Replace each 0 with a 0, each 1 with a 1, each 6 with a 9, each 8 with an 8, and each 9 with a 6.
+Constraints
+1
+≤
+|
+S
+|
+≤
+10
+5
+S
+ consists of 0, 1, 6, 8, and 9.
+
+*/
