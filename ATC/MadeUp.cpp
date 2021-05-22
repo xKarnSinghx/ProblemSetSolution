7d9c99bf0adfe8b3fd6934c6dbf0a20f987da119
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-22 23:56:28 +0530

    Create MadeUp.cpp

---
+++ ATC/MadeUp.cpp
@@ -0,0 +1,102 @@
+/*
+C - Made Up  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+Given are three sequences of length 
+N
+ each: 
+A
+=
+(
+A
+1
+,
+A
+2
+,
+…
+,
+A
+N
+)
+, 
+B
+=
+(
+B
+1
+,
+B
+2
+,
+…
+,
+B
+N
+)
+, and 
+C
+=
+(
+C
+1
+,
+C
+2
+,
+…
+,
+C
+N
+)
+, consisting of integers between 
+1
+ and 
+N
+ (inclusive).
+
+How many pairs 
+(
+i
+,
+j
+)
+ of integers between 
+1
+ and 
+N
+ (inclusive) satisfy 
+A
+i
+=
+B
+C
+j
+?
+
+Constraints
+1
+≤
+N
+≤
+10
+5
+1
+≤
+A
+i
+,
+B
+i
+,
+C
+i
+≤
+N
+All values in input are integers.
+*/
