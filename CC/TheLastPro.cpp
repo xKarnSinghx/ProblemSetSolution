92870ee7f2b3fb38b00e6d34401407bba9d5fe7a
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-24 02:37:24 +0530

    Create TheLastPro.cpp

---
+++ CC/TheLastPro.cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<int>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<ll, vll>             mvll;
+typedef  map<char, int>           mci;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+const ll MOD = 1000000007;
+void solve();
+
+ll N = 1001;
+ll a[1111][1111] = {};
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+
+	int p = 0, r;
+	for (int i = 1; i <= 1000; i++) {
+		p = p + i;
+		a[i][1] = p;
+		for (int j = 2, r = i; j <= 1000; j++, r++) {
+			a[i][j] += a[i][j - 1] + r;
+		}
+	}
+	ll t ; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+
+}
+ll powmod(ll x, ll y) {
+	ll res = 1;
+	for (ll i = 0; i < y; i++) {
+		res = res * x % MOD;
+	}
+	return res;
+}
+void solve() {
+	ll x, y, n, b;
+	cin >> x >> y >> n >> b;
+	ll p = 0;
+	for (ll i = x; i <= n; i++)
+		p += a[i][y] ;
+	for (ll i = y + 1; i <= b; i++)
+		p += a[n][i] ;
+	cout << p;
+}
+
+
