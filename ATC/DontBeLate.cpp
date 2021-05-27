b129b8ba5909c788d1a017fab64ad55d46def4d6
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-27 23:57:58 +0530

    Create DontBeLate.cpp

---
+++ ATC/DontBeLate.cpp
@@ -0,0 +1,41 @@
+/*
+A - Don't be late  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+100
+ points
+
+Problem Statement
+Takahashi is meeting up with Aoki.
+
+They have planned to meet at a place that is 
+D
+ meters away from Takahashi's house in 
+T
+ minutes from now.
+
+Takahashi will leave his house now and go straight to the place at a speed of 
+S
+ meters per minute.
+
+Will he arrive in time?
+
+Constraints
+1
+≤
+D
+≤
+10000
+1
+≤
+T
+≤
+10000
+1
+≤
+S
+≤
+10000
+All values in input are integers.
+*/
