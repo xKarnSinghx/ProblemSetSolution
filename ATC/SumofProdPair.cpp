8fe1ff6fee21074b5ebaafef5c63f9bbda25574d
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-27 23:59:00 +0530

    Create SumofProdPair.cpp

---
+++ ATC/SumofProdPair.cpp
@@ -0,0 +1,67 @@
+/*
+C - Sum of product of pairs  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+Given are 
+N
+ integers 
+A
+1
+,
+…
+,
+A
+N
+.
+
+Find the sum of 
+A
+i
+×
+A
+j
+ over all pairs 
+(
+i
+,
+j
+)
+ such that 
+1
+≤
+i
+<
+j
+≤
+N
+, modulo 
+(
+10
+9
++
+7
+)
+.
+
+Constraints
+2
+≤
+N
+≤
+2
+×
+10
+5
+0
+≤
+A
+i
+≤
+10
+9
+All values in input are integers.*/
