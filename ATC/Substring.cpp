74fd8c5943c653505fa5e55338839d5049417bf7
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-27 23:58:29 +0530

    Create Substring.cpp

---
+++ ATC/Substring.cpp
@@ -0,0 +1,48 @@
+/*
+B - Substring  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+Given are two strings 
+S
+ and 
+T
+.
+
+Let us change some of the characters in 
+S
+ so that 
+T
+ will be a substring of 
+S
+.
+
+At least how many characters do we need to change?
+
+Here, a substring is a consecutive subsequence. For example, xxx is a substring of yxxxy, but not a substring of xxyxx.
+
+Constraints
+The lengths of 
+S
+ and 
+T
+ are each at least 
+1
+ and at most 
+1000
+.
+The length of 
+T
+ is at most that of 
+S
+.
+S
+ and 
+T
+ consist of lowercase English letters.
+
+*/
