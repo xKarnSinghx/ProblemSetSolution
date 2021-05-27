008e1574d5d925f06aa49f0b6ef146a1ba1c1d96
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-27 23:59:29 +0530

    Create Friends.cpp

---
+++ ATC/Friends.cpp
@@ -0,0 +1,81 @@
+/*
+D - Friends  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+400
+ points
+
+Problem Statement
+There are 
+N
+ persons called Person 
+1
+ through Person 
+N
+.
+
+You are given 
+M
+ facts that "Person 
+A
+i
+ and Person 
+B
+i
+ are friends." The same fact may be given multiple times.
+
+If 
+X
+ and 
+Y
+ are friends, and 
+Y
+ and 
+Z
+ are friends, then 
+X
+ and 
+Z
+ are also friends. There is no friendship that cannot be derived from the 
+M
+ given facts.
+
+Takahashi the evil wants to divide the 
+N
+ persons into some number of groups so that every person has no friend in his/her group.
+
+At least how many groups does he need to make?
+
+Constraints
+2
+≤
+N
+≤
+2
+×
+10
+5
+0
+≤
+M
+≤
+2
+×
+10
+5
+1
+≤
+A
+i
+,
+B
+i
+≤
+N
+A
+i
+≠
+B
+i
+*/
