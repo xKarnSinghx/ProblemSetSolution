0e04d0ca9561e7123dc5dace0fb3de49e3649a8f
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-29 23:58:14 +0530

    Create MaxAdd.cpp

---
+++ ATC/MaxAdd.cpp
@@ -0,0 +1,122 @@
+/*
+A - Max Add  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+400
+ points
+
+Problem Statement
+For a sequence 
+a
+=
+(
+a
+1
+,
+a
+2
+,
+a
+3
+,
+…
+,
+a
+k
+)
+, let 
+f
+(
+a
+)
+ be the sum of its elements after the following is done:
+
+For each 
+i
+=
+1
+,
+2
+,
+3
+,
+…
+,
+k
+, in this order, do the following operation:
+add the current maximum value of an element in 
+a
+ to 
+a
+i
+.
+You are given a sequence of length 
+N
+: 
+A
+=
+(
+A
+1
+,
+A
+2
+,
+A
+3
+,
+…
+,
+A
+N
+)
+.
+For each integer 
+k
+ between 
+1
+ and 
+N
+ (inclusive), find 
+f
+(
+a
+)
+ when 
+a
+=
+(
+A
+1
+,
+A
+2
+,
+A
+3
+,
+…
+,
+A
+k
+)
+.
+
+Constraints
+1
+≤
+N
+≤
+2
+×
+10
+5
+1
+≤
+A
+i
+≤
+10
+7
+All values in input are integers.*/
