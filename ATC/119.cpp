8df9a0f149bb5a8d0446e99fae1ba57f794b86ba
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-05-29 23:58:55 +0530

    Create 119.cpp

---
+++ ATC/119.cpp
@@ -0,0 +1,63 @@
+/*
+A - 119 × 2^23 + 1  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score: 
+300
+ points
+
+Problem Statement
+In problems on AtCoder, you are often asked to:
+
+find the answer modulo 
+998244353
+.
+
+Here, we have 
+998244353
+=
+119
+×
+2
+23
++
+1
+. Related to this, solve the following prolem:
+
+You are given an integer 
+N
+.
+Print the minimum possible value of 
+a
++
+b
++
+c
+ for a triple of non-negative integers 
+(
+a
+,
+b
+,
+c
+)
+ satisfying 
+N
+=
+a
+×
+2
+b
++
+c
+.
+
+Constraints
+1
+≤
+N
+≤
+10
+18
+N
+ is an integer.*/
