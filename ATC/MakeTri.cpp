69d5f142ee77670876b0f8357f4b29625ca456ad
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-01 23:59:48 +0530

    Create MakeTri.cpp

---
+++ ATC/MakeTri.cpp
@@ -0,0 +1,87 @@
+/*
+B - Making Triangle  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+We have sticks numbered 
+1
+,
+⋯
+,
+N
+. The length of Stick 
+i
+ 
+(
+1
+≤
+i
+≤
+N
+)
+ is 
+L
+i
+.
+
+In how many ways can we choose three of the sticks with different lengths that can form a triangle?
+
+That is, find the number of triples of integers 
+(
+i
+,
+j
+,
+k
+)
+ 
+(
+1
+≤
+i
+<
+j
+<
+k
+≤
+N
+)
+ that satisfy both of the following conditions:
+
+L
+i
+, 
+L
+j
+, and 
+L
+k
+ are all different.
+There exists a triangle whose sides have lengths 
+L
+i
+, 
+L
+j
+, and 
+L
+k
+.
+Constraints
+1
+≤
+N
+≤
+100
+1
+≤
+L
+i
+≤
+10
+9
+All values in input are integers.*/
