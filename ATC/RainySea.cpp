e8f186d0fd2d75d16921e660ebb0e5126f23b0ea
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-01 23:59:22 +0530

    Create RainySea.cpp

---
+++ ATC/RainySea.cpp
@@ -0,0 +1,31 @@
+/*
+A - Rainy Season  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+100
+ points
+
+Problem Statement
+We have weather records at AtCoder Town for some consecutive three days. A string of length 
+3
+, 
+S
+, represents the records - if the 
+i
+-th character is S, it means it was sunny on the 
+i
+-th day; if that character is R, it means it was rainy on that day.
+
+Find the maximum number of consecutive rainy days in this period.
+
+Constraints
+|
+S
+|
+=
+3
+Each character of 
+S
+ is S or R.
+ */
