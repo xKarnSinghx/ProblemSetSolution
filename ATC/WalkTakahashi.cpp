07984766ba6a30f5e4d995d376951143637376ca
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-02 00:00:25 +0530

    Create WalkTakahashi.cpp

---
+++ ATC/WalkTakahashi.cpp
@@ -0,0 +1,58 @@
+/*
+C - Walking Takahashi  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+Takahashi, who lives on the number line, is now at coordinate 
+X
+. He will make exactly 
+K
+ moves of distance 
+D
+ in the positive or negative direction.
+
+More specifically, in one move, he can go from coordinate 
+x
+ to 
+x
++
+D
+ or 
+x
+−
+D
+.
+
+He wants to make 
+K
+ moves so that the absolute value of the coordinate of the destination will be the smallest possible.
+
+Find the minimum possible absolute value of the coordinate of the destination.
+
+Constraints
+−
+10
+15
+≤
+X
+≤
+10
+15
+1
+≤
+K
+≤
+10
+15
+1
+≤
+D
+≤
+10
+15
+All values in input are integers.
+*/
