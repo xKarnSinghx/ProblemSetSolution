477f78b88990404c345cb97fdabf8e734b1c2d84
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-02 22:33:51 +0530

    Create MashMokhACM.cpp

---
+++ CF/MashMokhACM.cpp
@@ -0,0 +1,197 @@
+/*
+B. Mashmokh and ACM
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Mashmokh's boss, Bimokh, didn't like Mashmokh. So he fired him. Mashmokh decided to go to university and participate in ACM instead of finding a new job. He wants to become a member of Bamokh's team. In order to join he was given some programming tasks and one week to solve them. Mashmokh is not a very experienced programmer. Actually he is not a programmer at all. So he wasn't able to solve them. That's why he asked you to help him with these tasks. One of these tasks is the following.
+
+A sequence of l integers b1, b2, ..., bl (1 ≤ b1 ≤ b2 ≤ ... ≤ bl ≤ n) is called good if each number divides (without a remainder) by the next number in the sequence. More formally  for all i (1 ≤ i ≤ l - 1).
+
+Given n and k find the number of good sequences of length k. As the answer can be rather large print it modulo 1000000007 (109 + 7).
+
+Input
+The first line of input contains two space-separated integers n, k (1 ≤ n, k ≤ 2000).
+
+Output
+Output a single integer — the number of good sequences of length k modulo 1000000007 (109 + 7).
+
+Examples
+inputCopy
+3 2
+outputCopy
+5
+inputCopy
+6 4
+outputCopy
+39
+inputCopy
+2 1
+outputCopy
+2
+Note
+In the first sample the good sequences are: [1, 1], [2, 2], [3, 3], [1, 2], [1, 3].
+
+
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<ll>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<ll, vll>             mvll;
+typedef  map<char, ll>           mcl;
+typedef  unordered_map<char, ll>           umcl;
+typedef  unordered_map< ll, char>           umlc;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+typedef priority_queue<ll, vector<ll>, greater<ll>> prq;
+const ll MOD = 1000000007;
+const ll modx = 998244353;
+void solve();
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+//	ll t ; cin >> t;
+//	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+
+}
+ll powmod(ll x, ll y) {
+	ll res = 1;
+	for (ll i = 0; i < y; i++) {
+		res = res * x % MOD;
+	}
+	return res;
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)
+		return false;
+	if (n <= 3)
+		return true;
+	if (n % 2 == 0 || n % 3 == 0)
+		return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+ll kadanesAlgo(vll a)
+{
+	ll n = a.size();
+
+	ll currMax = 0;
+	ll mx = INT_MIN;
+
+	foi(n)
+	{
+		currMax += a[i];
+
+		if (currMax <= a[i])
+		{
+			currMax = a[i];
+		}
+
+		mx = max(currMax, mx);
+	}
+	return mx;
+}
+void solve() {
+	ll n, l;
+	cin >> n >> l;
+	ll dp[l + 1][n + 1] = {};
+	dp[0][1] = 1;
+	for (ll k = 1; k < l + 1; k++) {
+		for (ll i = 1; i < n + 1; i++) {
+			for (ll j = i; j <= n; j += i) {
+				dp[k][j] += dp[k - 1][i];
+				dp[k][j] %= MOD;
+			}
+		}
+	}
+	ll ans = 0;
+	foi(n + 1) {
+		ans += dp[l][i];
+		ans %= MOD;
+	}
+	cout << ans;
+
+}
