9bd9502325136608a8891b4f10639f70e27899ed
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-03 23:55:31 +0530

    Create Step.cpp

---
+++ ATC/Step.cpp
@@ -0,0 +1,41 @@
+/*
+C - Step  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+N
+ persons are standing in a row. The height of the 
+i
+-th person from the front is 
+A
+i
+.
+
+We want to have each person stand on a stool of some heights - at least zero - so that the following condition is satisfied for every person:
+
+Condition: Nobody in front of the person is taller than the person. Here, the height of a person includes the stool.
+
+Find the minimum total height of the stools needed to meet this goal.
+
+Constraints
+1
+≤
+N
+≤
+2
+×
+10
+5
+1
+≤
+A
+i
+≤
+10
+9
+All values in input are integers.
+*/
