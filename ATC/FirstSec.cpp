7d289144dcbdb6dd94d0a56cc539bb6e6ba9156d
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-03 23:59:41 +0530

    Create FirstSec.cpp

---
+++ ATC/FirstSec.cpp
@@ -0,0 +1,108 @@
+/*
+B - First Second  / 
+Time Limit: 3 sec / Memory Limit: 1024 MB
+
+Score : 
+700
+ points
+
+Problem Statement
+Limak can repeatedly remove one of the first two characters of a string, for example 
+a
+b
+c
+x
+y
+x
+→
+a
+c
+x
+y
+x
+→
+c
+x
+y
+x
+→
+c
+y
+x
+.
+
+You are given 
+N
+ different strings 
+S
+1
+,
+S
+2
+,
+…
+,
+S
+N
+. Among 
+N
+⋅
+(
+N
+−
+1
+)
+/
+2
+ pairs 
+(
+S
+i
+,
+S
+j
+)
+, in how many pairs could Limak obtain one string from the other?
+
+Constraints
+2
+≤
+N
+≤
+200
+000
+S
+i
+ consists of lowercase English letters a-z.
+S
+i
+≠
+S
+j
+1
+≤
+|
+S
+i
+|
+|
+S
+1
+|
++
+|
+S
+2
+|
++
+…
++
+|
+S
+N
+|
+≤
+10
+6
+
+*/
