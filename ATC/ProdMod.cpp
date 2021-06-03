062b0ce177ea40630ee66d25c60e9039f91f4251
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-04 00:00:18 +0530

    Create ProdMod.cpp

---
+++ ATC/ProdMod.cpp
@@ -0,0 +1,77 @@
+/*
+C - Product Modulo  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+800
+ points
+
+Problem Statement
+Let’s take a prime 
+P
+=
+200
+003
+. You are given 
+N
+ integers 
+A
+1
+,
+A
+2
+,
+…
+,
+A
+N
+. Find the sum of 
+(
+(
+A
+i
+⋅
+A
+j
+)
+mod
+P
+)
+ over all 
+N
+⋅
+(
+N
+−
+1
+)
+/
+2
+ unordered pairs of elements (
+i
+<
+j
+).
+
+Please note that the sum isn't computed modulo 
+P
+.
+
+Constraints
+2
+≤
+N
+≤
+200
+000
+0
+≤
+A
+i
+<
+P
+=
+200
+003
+All values in input are integers.
+*/
