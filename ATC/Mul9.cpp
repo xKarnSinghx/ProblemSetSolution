e5655b969b1bf9fbf34f4a0ac90410da40f04c23
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-03 23:53:24 +0530

    Create Mul9.cpp

---
+++ ATC/Mul9.cpp
@@ -0,0 +1,35 @@
+/*
+B - Multiple of 9  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+An integer 
+N
+ is a multiple of 
+9
+ if and only if the sum of the digits in the decimal representation of 
+N
+ is a multiple of 
+9
+.
+
+Determine whether 
+N
+ is a multiple of 
+9
+.
+
+Constraints
+0
+≤
+N
+<
+10
+200000
+N
+ is an integer.
+ */
