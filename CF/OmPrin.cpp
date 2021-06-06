fe7ea5ea5348aec55435eb789483acb24d5d7e16
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-07 03:19:07 +0530

    Update OmPrin.cpp

--- CF/OmPrin.cpp
+++ CF/OmPrin.cpp
@@ -43,3 +43,216 @@ f
 b
 
 */
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<ll>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef vector<plll>      vplll;
+typedef  vector<string> 	     vs;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<ll, vll>             mvll;
+typedef  map<char, ll>           mcl;
+typedef  unordered_map<char, ll>           umcl;
+typedef  unordered_map< ll, char>           umlc;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+typedef priority_queue<ll, vector<ll>, greater<ll>> prq;
+const ll MOD = 1000000007;
+const ll modx = 998244353;
+void solve();
+vs s1, s2, s3;
+void lex(ll x, ll y, string s) {
+	if (x == y) {
+		s1.push_back(s);
+		return;
+	}
+	foi(26) {
+		string t = s;
+		t += char('a' + i);
+		lex(x + 1, y, t);
+	}
+}
+void lex1(ll x, ll y, string s) {
+	if (x == y) {
+		s2.push_back(s);
+		return;
+	}
+	foi(26) {
+		string t = s;
+		t += char('a' + i);
+		lex1(x + 1, y, t);
+	}
+}
+void lex2(ll x, ll y, string s) {
+	if (x == y) {
+		s3.push_back(s);
+		return;
+	}
+	foi(26) {
+		string t = s;
+		t += char('a' + i);
+		lex2(x + 1, y, t);
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+	lex(0, 1, "");
+	lex1(0, 2, "");
+	lex2(0, 3, "");
+	ll t ; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+
+}
+ll powmod(ll x, ll y) {
+	ll res = 1;
+	for (ll i = 0; i < y; i++) {
+		res = res * x % MOD;
+	}
+	return res;
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)
+		return false;
+	if (n <= 3)
+		return true;
+	if (n % 2 == 0 || n % 3 == 0)
+		return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+ll kadanesAlgo(vll a)
+{
+	ll n = a.size();
+
+	ll currMax = 0;
+	ll mx = INT_MIN;
+
+	foi(n)
+	{
+		currMax += a[i];
+
+		if (currMax <= a[i])
+		{
+			currMax = a[i];
+		}
+
+		mx = max(currMax, mx);
+	}
+	return mx;
+}
+void solve() {
+	ll n;
+	cin >> n;
+	string s;
+	cin >> s;
+	sort(s1.begin(), s1.end());
+	sort(s2.begin(), s2.end());
+	sort(s3.begin(), s3.end());
+	set<string> st;
+	foi(n) {
+		foj(4) {
+			if (j == 0)
+				continue;
+			st.insert(s.substr(i, j));
+		}
+	}
+	foi(s1.size()) {
+		if (!st.count(s1[i])) {
+			cout << s1[i];
+			return;
+		}
+	}
+	foi(s2.size()) {
+		if (!st.count(s2[i])) {
+			cout << s2[i];
+			return;
+		}
+	}
+	foi(s3.size()) {
+		if (!st.count(s3[i])) {
+			cout << s3[i];
+			return;
+		}
+	}
+
+
+}
