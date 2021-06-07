d2ea8e6dede1f2f8601d182e72f901bbcd5f7c07
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-07 23:55:05 +0530

    Create JudgeStat.cpp

---
+++ ATC/JudgeStat.cpp
@@ -0,0 +1,41 @@
+/*
+B - Judge Status Summary  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+Takahashi is participating in a programming contest called AXC002, and he has just submitted his code to Problem A.
+
+The problem has 
+N
+ test cases.
+
+For each test case 
+i
+ (
+1
+≤
+i
+≤
+N
+), you are given a string 
+S
+i
+ representing the verdict for that test case. Find the numbers of test cases for which the verdict is AC, WA, TLE, and RE, respectively.
+
+See the Output section for the output format.
+
+Constraints
+1
+≤
+N
+≤
+10
+5
+S
+i
+ is AC, WA, TLE, or RE.
+ */
