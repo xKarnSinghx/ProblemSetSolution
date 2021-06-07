c1022887de1923e1cf134b1d67704ce4a44b45fb
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-07 23:54:38 +0530

    Create Payment.cpp

---
+++ ATC/Payment.cpp
@@ -0,0 +1,27 @@
+/*
+A - Payment  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+100
+ points
+
+Problem Statement
+We will buy a product for 
+N
+ yen (the currency of Japan) at a shop.
+
+If we use only 
+1000
+-yen bills to pay the price, how much change will we receive?
+
+Assume we use the minimum number of bills required.
+
+Constraints
+1
+≤
+N
+≤
+10000
+N
+ is an integer.*/
