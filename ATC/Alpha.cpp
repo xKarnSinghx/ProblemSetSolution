358d48522e81c86e4800c941f50b096c6c1de382
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-08 23:58:51 +0530

    Create Alpha.cpp

---
+++ ATC/Alpha.cpp
@@ -0,0 +1,19 @@
+/*
+A - αlphabet  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+100
+ points
+
+Problem Statement
+An uppercase or lowercase English letter 
+α
+ will be given as input. If 
+α
+ is uppercase, print A; if it is lowercase, print a.
+
+Constraints
+α
+ is an uppercase (A - Z) or lowercase (a - z) English letter
+ */
