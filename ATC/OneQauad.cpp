712e5bb600ce4ff27a2b16ad96d1fe7c533d4e71
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-08 23:59:45 +0530

    Create OneQauad.cpp

---
+++ ATC/OneQauad.cpp
@@ -0,0 +1,96 @@
+/*
+C - One Quadrillion and One Dalmatians  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+300
+ points
+
+Problem Statement
+1000000000000001
+ dogs suddenly appeared under the roof of Roger's house, all of which he decided to keep. The dogs had been numbered 
+1
+ through 
+1000000000000001
+, but he gave them new names, as follows:
+
+the dogs numbered 
+1
+,
+2
+,
+⋯
+,
+26
+ were respectively given the names a, b, ..., z;
+the dogs numbered 
+27
+,
+28
+,
+29
+,
+⋯
+,
+701
+,
+702
+ were respectively given the names aa, ab, ac, ..., zy, zz;
+the dogs numbered 
+703
+,
+704
+,
+705
+,
+⋯
+,
+18277
+,
+18278
+ were respectively given the names aaa, aab, aac, ..., zzy, zzz;
+the dogs numbered 
+18279
+,
+18280
+,
+18281
+,
+⋯
+,
+475253
+,
+475254
+ were respectively given the names aaaa, aaab, aaac, ..., zzzy, zzzz;
+the dogs numbered 
+475255
+,
+475256
+,
+⋯
+ were respectively given the names aaaaa, aaaab, ...;
+and so on.
+To sum it up, the dogs numbered 
+1
+,
+2
+,
+⋯
+ were respectively given the following names:
+
+a, b, ..., z, aa, ab, ..., az, ba, bb, ..., bz, ..., za, zb, ..., zz, aaa, aab, ..., aaz, aba, abb, ..., abz, ..., zzz, aaaa, ...
+
+Now, Roger asks you:
+
+"What is the name for the dog numbered 
+N
+?"
+
+Constraints
+N
+ is an integer.
+1
+≤
+N
+≤
+1000000000000001*/
