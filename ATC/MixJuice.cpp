157e3fcacf0d341eac9929802a26418739059a06
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-08 23:59:16 +0530

    Create MixJuice.cpp

---
+++ ATC/MixJuice.cpp
@@ -0,0 +1,48 @@
+/*
+B - Mix Juice  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+200
+ points
+
+Problem Statement
+A shop sells 
+N
+ kinds of fruits, Fruit 
+1
+,
+…
+,
+N
+, at prices of 
+p
+1
+,
+…
+,
+p
+N
+ yen per item, respectively. (Yen is the currency of Japan.)
+
+Here, we will choose 
+K
+ kinds of fruits and buy one of each chosen kind. Find the minimum possible total price of those fruits.
+
+Constraints
+1
+≤
+K
+≤
+N
+≤
+1000
+1
+≤
+p
+i
+≤
+1000
+All values in input are integers.
+
+*/
