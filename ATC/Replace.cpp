bb0d7b145db9a0fb54623e6925d02af56d6ae7eb
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-06-09 00:00:07 +0530

    Create Replace.cpp

---
+++ ATC/Replace.cpp
@@ -0,0 +1,83 @@
+/*
+D - Replacing  / 
+Time Limit: 2 sec / Memory Limit: 1024 MB
+
+Score : 
+400
+ points
+
+Problem Statement
+You have a sequence 
+A
+ composed of 
+N
+ positive integers: 
+A
+1
+,
+A
+2
+,
+⋯
+,
+A
+N
+.
+
+You will now successively do the following 
+Q
+ operations:
+
+In the 
+i
+-th operation, you replace every element whose value is 
+B
+i
+ with 
+C
+i
+.
+For each 
+i
+ 
+(
+1
+≤
+i
+≤
+Q
+)
+, find 
+S
+i
+: the sum of all elements in 
+A
+ just after the 
+i
+-th operation.
+
+Constraints
+All values in input are integers.
+1
+≤
+N
+,
+Q
+,
+A
+i
+,
+B
+i
+,
+C
+i
+≤
+10
+5
+B
+i
+≠
+C
+i
+*/
