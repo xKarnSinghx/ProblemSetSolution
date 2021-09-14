0a14232406b46757342dfe9e1765a8008e532608
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-09-14 22:28:38 +0530

    Update Buds.cpp

--- CFGdPr/Buds.cpp
+++ CFGdPr/Buds.cpp
@@ -84,3 +84,255 @@ You can choose a bud vertex 3 and re-hang it to vertex 5. As a result, you will 
 It can be proved that it is the minimal number of leaves possible to get.
 
 */
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<ll>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef queue<ll>           qll;
+typedef vector<plll>      vplll;
+typedef  vector<set<ll>>   vsll;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<ll, vll>             mvll;
+typedef  map<char, ll>           mcl;
+typedef map<pair<ll, ll>, ll>  mplll;
+typedef  unordered_map<char, ll>           umcl;
+typedef  unordered_map< ll, char>           umlc;
+typedef  unordered_map< ll, ld>           umld;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+typedef priority_queue<ll, vector<ll>, greater<ll>> prq;
+const ll MOD = 1000000007;
+const ll modx = 998244353;
+ld PI = 3.1415926535897;
+const ll N = 200010;
+ull pows(ull x1, ll y1, ll mod)
+{
+	ull r1 = 1;
+	x1 = x1 % mod;
+	while (y1 > 0)	{
+		if (y1 & 1)
+			r1 = (r1 * x1) % mod;
+		y1 = y1 >> 1;
+		x1 = (x1 * x1) % mod;
+	}
+	return r1;
+}
+ull f[N];
+ull mi(ull x, ll mod) {
+	return pows(x, mod - 2, mod);
+}
+ull ncr(ull x, ll y, ll mod) {
+	if (x < y)
+		return 0;
+	if (y == 0)
+		return 1;
+	ll ans = f[x] * mi(f[y], mod) % mod;
+	ll z = (ans * mi(f[x - y], mod) % mod) % mod;
+	return z % mod;
+}
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+	ll t ; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+
+}
+ll powmod(ll x, ll y) {
+	ll res = 1;
+	for (ll i = 0; i < y; i++) {
+		res = res * x % MOD;
+	}
+	return res;
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)
+		return false;
+	if (n <= 3)
+		return true;
+	if (n % 2 == 0 || n % 3 == 0)
+		return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+bool COMP(pll l, pll r) {
+	return l.ss < r.ss;
+}
+ll kadanesAlgo(vll a)
+{
+	ll n = a.size();
+
+	ll currMax = 0;
+	ll mx = INT_MIN;
+
+	foi(n)
+	{
+		currMax += a[i];
+
+		if (currMax <= a[i])
+		{
+			currMax = a[i];
+		}
+
+		mx = max(currMax, mx);
+	}
+	return mx;
+}
+string ask(ll x, ll y) {
+	cout << x + 1 << " " << y + 1;
+	string s;
+	cin >> s;
+	return s;
+}
+ll binpow(ll a, ll b, ll m) {
+	a %= m;
+	ll res = 1;
+	while (b > 0) {
+		if (b & 1)
+			res = res * a % m;
+		a = a * a % m;
+		b >>= 1;
+	}
+	return res;
+}
+bool is_sorted(vector<ll>a) {
+	ll x = 0;
+	for (ll i = 0; i < a.size() - 1; i++) {
+		if (a[i] < a[i + 1])
+			x++;
+	}
+	return x == a.size() - 1;
+}
+bool check(string a, string b) {
+	ll n = a.size(), m = b.size();
+	if (a < b) {
+		return true;
+	}
+	if (a > b) {
+		return false;
+	}
+	foi(a.size()) {
+		if (a[i] > b[i]) {
+			return false;
+		}
+		else if (a[i] < b[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+bool isSubSequence(string a, string b, ll m, ll n) {
+	ll j = 0;
+	for (ll i = 0; i < n and j < m; i++)
+		if (a[j] == b[i])
+			j++;
+	return (j == m);
+}
+ll ans = 0, n;
+vll g[200001];
+bool dfs(ll u, ll p) {
+	ll leaf = 0;
+	for (auto it : g[u]) {
+		if (it != p)
+			leaf += !dfs(it, u);
+	}
+	ans += max(0ll, leaf - 1);
+	return leaf != 0;
+}
+void solve() {
+	cin >> n;
+	foi(n - 1) {
+		ll x, y;
+		cin >> x >> y;
+		x--, y--;
+		g[x].pb(y);
+		g[y].pb(x);
+	}
+	dfs(0, -1);
+	cout << ans + 1;
+	ans = 0;
+	foi(200001) {
+		g[i].clear();
+	}
+}
+
+
+ 
