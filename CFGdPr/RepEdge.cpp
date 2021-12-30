1f34861422e0e58a729a63629ef6f643e25e11be
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-12-30 23:46:06 +0530

    Create RepEdge.cpp

---
+++ CFGdPr/RepEdge.cpp
@@ -0,0 +1,288 @@
+/*
+C. Representative Edges
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+An array a1,a2,…,an is good if and only if for every subsegment 1≤l≤r≤n, the following holds: al+al+1+…+ar=12(al+ar)⋅(r−l+1).
+
+You are given an array of integers a1,a2,…,an. In one operation, you can replace any one element of this array with any real number. Find the minimum number of operations you need to make this array good.
+
+Input
+The first line of input contains one integer t (1≤t≤100): the number of test cases.
+
+Each of the next t lines contains the description of a test case.
+
+In the first line you are given one integer n (1≤n≤70): the number of integers in the array.
+
+The second line contains n integers a1,a2,…,an (−100≤ai≤100): the initial array.
+
+Output
+For each test case, print one integer: the minimum number of elements that you need to replace to make the given array good.
+
+Example
+inputCopy
+5
+4
+1 2 3 4
+4
+1 1 2 2
+2
+0 -1
+6
+3 -2 4 -1 -4 0
+1
+-100
+outputCopy
+0
+2
+0
+3
+0
+Note
+In the first test case, the array is good already.
+
+In the second test case, one of the possible good arrays is [1,1,1–,1–] (replaced elements are underlined).
+
+In the third test case, the array is good already.
+
+In the fourth test case, one of the possible good arrays is [−2.5–––––,−2,−1.5–––––,−1,−0.5–––––,0].
+
+
+*/
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<ll>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<ld>          vld;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef queue<ll>           qll;
+typedef vector<plll>      vplll;
+typedef  vector<set<ll>>   vsll;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<ll, vll>             mvll;
+typedef  map<vll, ll>             mvlll;
+typedef  map<char, ll>           mcl;
+typedef map<pair<ll, ll>, ll>  mplll;
+typedef  unordered_map<char, ll>           umcl;
+typedef  unordered_map< ll, char>           umlc;
+typedef  unordered_map< ll, ld>           umld;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+typedef priority_queue<ll, vector<ll>, greater<ll>> prq;
+const ll MOD = 1000000007;
+const ll mod = 998244353;
+ld PI = 3.1415926535897;
+const ll N = 1000010;
+void solve();
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+	ll t; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+
+}
+ll powmod(ll x, ll y) {
+	ll res = 1;
+	for (ll i = 0; i < y; i++) {
+		res = res * x % MOD;
+	}
+	return res;
+}
+bool isPrime(ll n)
+{
+	if (n <= 1)
+		return false;
+	if (n <= 3)
+		return true;
+	if (n % 2 == 0 || n % 3 == 0)
+		return false;
+
+	for (ll i = 5; i * i <= n; i = i + 6)
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+
+	return true;
+}
+bool COMP(pll l, pll r) {
+	return l.ss < r.ss;
+}
+ll kadanesAlgo(vll a)
+{
+	ll n = a.size();
+
+	ll currMax = 0;
+	ll mx = INT_MIN;
+
+	foi(n)
+	{
+		currMax += a[i];
+
+		if (currMax <= a[i])
+		{
+			currMax = a[i];
+		}
+
+		mx = max(currMax, mx);
+	}
+	return mx;
+}
+ll ask(ll x, ll y) {
+	cout << "?" << " ";
+	cout << x << " " << y << endl;
+	ll s;
+	cin >> s;
+	return s;
+}
+ll binpow(ll a, ll b, ll m) {
+	a %= m;
+	ll res = 1;
+	while (b > 0) {
+		if (b & 1)
+			res = res * a % m;
+		a = a * a % m;
+		b >>= 1;
+	}
+	return res;
+}
+bool is_sorted(vector<ll>a) {
+	ll x = 0;
+	for (ll i = 0; i < a.size() - 1; i++) {
+		if (a[i] < a[i + 1])
+			x++;
+	}
+	return x == a.size() - 1;
+}
+bool check(string a, string b) {
+	ll n = a.size(), m = b.size();
+	if (a < b) {
+		return true;
+	}
+	if (a > b) {
+		return false;
+	}
+	foi(a.size()) {
+		if (a[i] > b[i]) {
+			return false;
+		}
+		else if (a[i] < b[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+bool isSubSequence(string a, string b, ll m, ll n) {
+	ll j = 0;
+	for (ll i = 0; i < n and j < m; i++)
+		if (a[j] == b[i])
+			j++;
+	return (j == m);
+}
+ll power(ll n, ll x) {
+	ll ans = 1;
+	foi(n) {
+		ans *= x;
+	}
+	return ans;
+}
+void solve() {
+	ll n;
+	cin >> n;
+	vld a(n);
+	foi(n) {
+		cin >> a[i];
+	}
+	if (n == 1 or n == 2) {
+		cout << 0;
+		return;
+	}
+	ll ans = n;
+	foi(n - 1) {
+		for (ll j = i + 1; j < n; j++) {
+			ll y = 0;
+			fok(n) {
+				if (k == i) {
+					continue;
+				}
+				if (a[k] * (j - i) + (a[j] - a[i]) * (i - k) != a[i] * (j - i)) {
+					y++;
+				}
+			}
+			ans = min(ans, y);
+		}
+	}
+	cout << ans;;
+}
+
