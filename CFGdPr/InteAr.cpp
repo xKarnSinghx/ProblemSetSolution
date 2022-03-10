45266d10482ec1c151e5cedd8addbe8bc11000ab
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2022-03-10 22:22:12 +0530

    Update InteAr.cpp

--- CFGdPr/InteAr.cpp
+++ CFGdPr/InteAr.cpp
@@ -59,3 +59,176 @@ In the second test case it is enough to see that
 
 In the third test case ⌊22⌋=1, but there is only 2 in the array, that's why it is not integral.
 */
+#include<bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned ll
+#define mp                   make_pair
+#define eb                   emplace_back
+#define pb                   push_back
+#define pf                   push_front
+#define popb                 pop_back
+#define popf                 pop_front
+#define hashmap              unordered_map
+#define hashset              unordered_set
+#define lb                   lower_bound
+#define ub                   upper_bound
+#define all(a)               (a).begin(), (a).end()
+#define rall(a)              (a).rbegin(), (a).rend()
+#define ff                   first
+#define ss                   second
+#define foi(n) for(ll i=0;i<n;i++)
+#define foj(n) for(ll j=0;j<n;j++)
+#define fok(n) for(ll k=0;k<n;k++)
+#define forr(i,a,b) for(ll i=a;i<b;i++)
+#define forrr(i,b,a) for(ll i=b;i>=a;i--)
+#define forrrr(i,a,b,k) for(ll i=a;i<b;i=i+k)
+#define graph          vector<vector<ll>>
+#define sz(v) v.size()
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pll;
+typedef vector<int>         vi;
+typedef vector<ll>          vll;
+typedef vector<ld>          vld;
+typedef vector<string>      vs;
+typedef vector<double>      vd;
+typedef vector<pii>         vpii;
+typedef vector<pll>         vpll;
+typedef pair< ll, pll>      plll;
+typedef queue<ll>           qll;
+typedef vector<plll>      vplll;
+typedef  vector<set<ll>>   vsll;
+typedef  vector<char>	         vc;
+typedef  vector<bool>            vb;
+typedef  map<string, int>         msi;
+typedef  map<int, int>	         mii;
+typedef  map<ll, ll>             mll;
+typedef  map<ll, vll>             mvll;
+typedef  map<vll, ll>             mvlll;
+typedef  map<char, ll>           mcl;
+typedef map<pair<ll, ll>, ll>  mplll;
+typedef  unordered_map<char, ll>           umcl;
+typedef  unordered_map< ll, char>           umlc;
+typedef  unordered_map< ll, ld>           umld;
+typedef  map<int, string>	     mis;
+typedef  pair<string, int>       psi;
+typedef  pair<string, string>    pss;
+typedef priority_queue <ll> pq;
+typedef priority_queue<pii, vector<pii>, greater<pii> > pqq;
+typedef priority_queue<ll, vector<ll>, greater<ll>> prq;
+const ll MOD = 1000000007;
+const ll mod = 998244353;
+ld PI = 3.1415926535897;
+const ll N = 1000010;
+void solve();
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("error1.txt", "w", stderr);
+	freopen("output1.txt", "w", stdout);
+#endif
+	ll t; cin >> t;
+	while (t--)
+	{
+		solve();
+		cout << "\n";
+	}
+
+	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+	return 0;
+}
+ll ceils(ll x, ll y) {
+	return x / y + ((x % y) != 0);
+}
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+
+}
+ll binpow(ll a, ll b, ll m) {
+	a %= m;
+	ll res = 1;
+	while (b > 0) {
+		if (b & 1)
+			res = res * a % m;
+		a = a * a % m;
+		b >>= 1;
+	}
+	return res;
+}
+string ans(string &s) {
+	map<int, vector<char>>m;
+	m[1] = {',', '@'};
+	m[2] = {'A', 'B', 'C', 'a', 'b', 'c', '2'};
+	m[3] = {'D', 'E', 'F', 'd', 'e', 'f', '3'};
+	m[4] = {'G', 'H', 'I', 'g', 'h', 'i', '4'};
+	m[5] = {'J', 'K', 'L', 'j', 'k', 'l', '5'};
+	m[6] = {'M', 'N', 'O', 'm', 'n', 'o', '6'};
+	m[7] = {'P', 'Q', 'R', 'S', 'p', 'q', 'r', 's', '7'};
+	m[8] = {'T', 'U', 'V', 't', 'u', 'v', '8'};
+	m[9] = {'X', 'Y', 'Z', 'x', 'y', 'z', '9'};
+	m[0] = {' ', '0'};
+	string ans = "";
+	int c = s[0] - '0', r = 1;
+	for (int i = 1; i < s.size(); i++) {
+		if (s[i] == '_' or i == s.size() - 1 or s[i] != s[i - 1]) {
+			if (s[i] == s[i - 1])
+				ans += m[c][r % m[c].size()];
+			else if (s[i] != s[i - 1] and s[i] != '_')
+				ans += m[c][(r - 1) % m[c].size()];
+			if (isdigit(s[i]))
+				c = s[i] - '0', r = 1;
+			if (i == s.size() - 1 and s[i] == s[i - 1])
+				return ans;
+		}
+		else {
+			r++;
+		}
+	}
+	ans += m[c][r - 1];
+	return ans;
+
+}
+void solve() {
+	ll n, c;
+	cin >> n >> c;
+	vll a(n), m(2 * c + 1, 0);
+	foi(n) {
+		cin >> a[i];
+		m[a[i]]++;
+	}
+	for (ll i = 2; i < 2 * c + 1; i++) {
+		m[i] += m[i - 1];
+	}
+	if (!m[1]) {
+		cout << "NO";
+		return;
+	}
+	foi(c + 1) {
+		if (i == 0 or m[i] == m[i - 1] or i == 1)
+			continue;
+		for (ll j = i; j < c + 1; j += i) {
+			if (m[i + j - 1] > m[j - 1] and m[j / i] == m[j / i - 1]) {
+				cout << "NO";
+				return;
+			}
+		}
+	}
+	cout << "YES";
+
+}
