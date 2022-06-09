0239aed3ca441ecd098ef9020cc219a8d185fe42
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2022-06-09 23:23:25 +0530

    Create AlmostId.cpp

---
+++ CFGdPr/AlmostId.cpp
@@ -0,0 +1,37 @@
+/*
+D. Almost Identity Permutations
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+A permutation p of size n is an array such that every integer from 1 to n occurs exactly once in this array.
+
+Let's call a permutation an almost identity permutation iff there exist at least n - k indices i (1 ≤ i ≤ n) such that pi = i.
+
+Your task is to count the number of almost identity permutations for given numbers n and k.
+
+Input
+The first line contains two integers n and k (4 ≤ n ≤ 1000, 1 ≤ k ≤ 4).
+
+Output
+Print the number of almost identity permutations for given n and k.
+
+Examples
+inputCopy
+4 1
+outputCopy
+1
+inputCopy
+4 2
+outputCopy
+7
+inputCopy
+5 3
+outputCopy
+31
+inputCopy
+5 4
+outputCopy
+76
+
+*/
